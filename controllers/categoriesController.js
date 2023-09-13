e2c36a6711bed7dc3ca991ed41640870a9a25c96
Collins Kipruto <collinskandie1@gmail.com>
2023-09-13 04:07:11 +0300

    wow

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -53,22 +53,64 @@ const deleteCategory = async (req, res) => {
 };
 const editCategory = async (req, res) => {
   try {
+    const { categoryId, name, description } = req.body;
     console.log(req.body);
-    const { categoryId, name, image } = req.body;
-    console.log(categoryId);
-    const category = await Category.findByPk(categoryId);
 
-    if (!category) {
-      console.log("Category not found");
-      return res.status(404).json({ error: "Category not found" });
+    // Handle image upload and renaming
+    if (req.file) {
+      const imageFile = req.file;
+      const newImageName = `${name.charAt(0).toUpperCase()}${name.substring(
+        1
+      )}_${Date.now()}.jpeg`;
+
+      // Rename and save the image to the public/img/uploads folder
+      imageFile.mv(`public/img/uploads/${newImageName}`, (err) => {
+        if (err) {
+          console.error("Error renaming and saving image:", err);
+          return res
+            .status(500)
+            .json({ error: "Error updating category image" });
+        }
+
+        // Update the imageUrl, name, and description fields in the Category model
+        Category.update(
+          { imageUrl: newImageName, name: name, description: description },
+          { where: { id: categoryId } }
+        )
+          .then(() => {
+            console.log("Category updated successfully");
+            return res
+              .status(200)
+              .json({ message: "Category updated successfully" });
+          })
+          .catch((error) => {
+            console.error("Error updating Category model:", error);
+            return res.status(500).json({ error: "Error updating category" });
+          });
+      });
+    } else {
+      // If no image is uploaded, update the name and description fields in the Category model
+      Category.update(
+        { name: name, description: description },
+        { where: { id: categoryId } }
+      )
+        .then(() => {
+          console.log("Category updated successfully");
+          return res
+            .status(200)
+            .json({ message: "Category updated successfully" });
+        })
+        .catch((error) => {
+          console.error("Error updating Category model:", error);
+          return res.status(500).json({ error: "Error updating category" });
+        });
     }
-    return res.status(404).json({ success: "Category found" });
-    // await category.update;
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).json({ error: "Error editing category" });
   }
 };
+
 module.exports = {
   getAllCategories,
   AddCategories,
